35014921cb91455eb57cc7e3544f631a48be6396
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-09-10 00:26:04 +0900

    add deps

--- mix.exs
+++ mix.exs
@@ -27,6 +27,9 @@ defmodule QuickcheckExample.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:excheck, "~> 0.5", only: :test},
+      {:triq, github: "triqng/triq", only: :test}
+    ]
   end
 end
