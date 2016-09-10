f586a3ff7d266f7debc460a9cabd191c1b3b0f8f
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-09-10 11:45:57 +0900

    add example

--- lib/quickcheck_example.ex
+++ lib/quickcheck_example.ex
@@ -1,2 +1,22 @@
 defmodule QuickcheckExample do
+  @moduledoc """
+
+  ## Example
+
+      iex> QuickcheckExample.sort([3,6,7,8,2,1])
+      [1,2,3,6,7,8]
+
+  """
+
+  @spec sort([any]) :: [any]
+  def sort([]), do: []
+  def sort([h | t]) do
+    smaller = t |> Enum.filter(&(&1 <= h))
+    greater = t |> Enum.filter(&(&1 > h))
+
+    s_sort = sort(smaller)
+    g_sort = sort(greater)
+
+    [s_sort | [h | g_sort]] |> List.flatten
+  end
 end
--- test/quickcheck_example_test.exs
+++ test/quickcheck_example_test.exs
@@ -1,8 +1,46 @@
 defmodule QuickcheckExampleTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use ExCheck
   doctest QuickcheckExample
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  property :sort_is_idempotent do
+    for_all x in list(int) do
+      sorted = QuickcheckExample.sort(x)
+      QuickcheckExample.sort(x) == sorted
+    end
+  end
+
+  property :single_element_list_is_sorted do
+    for_all x in int do
+      QuickcheckExample.sort([x]) == [x]
+    end
+  end
+
+  property :head_less_eql_to_tail do
+    for_all x in list(int) do
+      implies x != [] do
+        sorted = QuickcheckExample.sort(x)
+        hd(sorted) <= List.last(sorted)
+      end
+    end
+  end
+
+  property :head_less_eql_to_tail_two do
+    for_all x in such_that(x in list(int) when x != []) do
+      sorted = QuickcheckExample.sort(x)
+      hd(sorted) <= List.last(sorted)
+    end
+  end
+
+  property :sorts_integers do
+    for_all x in list(int) do
+      QuickcheckExample.sort(x) == Enum.sort(x)
+    end
+  end
+
+  property :sorts_real_numbers do
+    for_all x in list(real) do
+      QuickcheckExample.sort(x) == Enum.sort(x)
+    end
   end
 end
